4b882e4a3cea868595f9afced5a9cb66aec504d6
Atendra Kumar Kushwaha <atendrakushwaha9685@gmail.com>
2025-09-04 17:02:02 +0530

    04/09/2025-9

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-plugins: [react()],
-})
+  plugins: [react()],
+  preview: {
+    host: true, // allow all hosts
+    allowedHosts: ["vaidhei-frontend.onrender.com"], // add your render domain
+  },
+});
