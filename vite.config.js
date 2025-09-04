de81ec131c531bd3f4f58860ce8ec393eac6d778
Atendra Kumar Kushwaha <atendrakushwaha9685@gmail.com>
2025-09-04 16:09:36 +0530

    04/09/2025-3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./", // ensures correct paths for Vercel deployment
-  server: {
-    port: 5173,
-  },
-});
+plugins: [react()],
+})
