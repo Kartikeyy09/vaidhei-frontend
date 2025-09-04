bf50fc9cb629687e8be7cd7e8bb96b37d3187d76
Atendra Kumar Kushwaha <atendrakushwaha9685@gmail.com>
2025-09-04 18:05:51 +0530

    04/09/2025-11

--- src/commonPages/pages/NotFoundPage.jsx
+++ src/commonPages/pages/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { HomeIcon, ArrowUturnLeftIcon } from '@heroicons/react/24/outline';
 import { motion, useMotionValue, useTransform } from 'framer-motion';
 import Lottie from "lottie-react"
-import NotFound from '../../data/Questioning.json'
+import NotFound from '../../lotify-icone/Questioning.json'
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
