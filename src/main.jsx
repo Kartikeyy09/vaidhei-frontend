55bbd21a9494fab166d54ca6ce6bff3842cf03dd
Atendra Kumar Kushwaha <atendrakushwaha9685@gmail.com>
2025-09-04 15:39:43 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
 import { store } from "./store/store";
 import { Provider } from "react-redux";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import { BrowserRouter } from "react-router-dom"; // Add BrowserRouter
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <ToastContainer />
-      </BrowserRouter>
+    <App />
+    <ToastContainer />
     </Provider>
   </React.StrictMode>
 );
