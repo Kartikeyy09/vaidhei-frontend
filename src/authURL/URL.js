a33d1199f40f325d832ca7d44e676a5e73d25da4
dadore1803 <57829128+dadore1803@users.noreply.github.com>
2025-10-28 19:45:04 +0530

    / added before api

--- src/authURL/URL.js
+++ src/authURL/URL.js
@@ -3,31 +3,31 @@
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // ---------------- login ----------------
-export const ADMIN_LOGIN_URL = `${BASE_URL}api/auth`;
+export const ADMIN_LOGIN_URL = `${BASE_URL}/api/auth`;
 
-export const PROFILE_API_URL = `${BASE_URL}api/profile`;
+export const PROFILE_API_URL = `${BASE_URL}/api/profile`;
 
-export const SERVICE_URL = `${BASE_URL}api/services`;
+export const SERVICE_URL = `${BASE_URL}/api/services`;
 
-export const PROJECT_URL = `${BASE_URL}api/projects`;
+export const PROJECT_URL = `${BASE_URL}/api/projects`;
 
-export const TENDER_URL = `${BASE_URL}api/tenders`;
+export const TENDER_URL = `${BASE_URL}/api/tenders`;
 
-export const GALLERY_URL = `${BASE_URL}api/gallery`;
-
-
-export const INQUIRY_URL = `${BASE_URL}api/inquiries`;
-
-export const DASHBOARD_URL = `${BASE_URL}api/dashboard/stats`; 
+export const GALLERY_URL = `${BASE_URL}/api/gallery`;
 
 
-export const JOURNEY_URL = `${BASE_URL}api/journey`;
+export const INQUIRY_URL = `${BASE_URL}/api/inquiries`;
 
-export const TESTIMONIAL_URL = `${BASE_URL}api/testimonials`;
+export const DASHBOARD_URL = `${BASE_URL}/api/dashboard/stats`; 
 
-export const SETTINGS_URL = `${BASE_URL}api/settings`;
 
-export const USER_URL = `${BASE_URL}api/users/`;
+export const JOURNEY_URL = `${BASE_URL}/api/journey`;
+
+export const TESTIMONIAL_URL = `${BASE_URL}/api/testimonials`;
+
+export const SETTINGS_URL = `${BASE_URL}/api/settings`;
+
+export const USER_URL = `${BASE_URL}/api/users/`;
 
 export const INVOICE_URL = `${BASE_URL}api/invoices`;
 
